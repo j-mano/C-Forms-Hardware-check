a6473b3e827522f2772677fa6be39e4e61dc67ff
joachim <Joachim@johnsons.nu>
2021-02-27 16:52:18 +0100

    Inital start upp.
    Start of the project
    interface added
    decisions made for witch hardware to show.

---
+++ FrontEnd.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hardware
+{
+    public partial class FrontEnd : Form
+    {
+        public FrontEnd()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label23_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
